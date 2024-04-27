e83fd0919038d3236ac29e8bf25aa5a52060a40b
izmyname <135810812+izmyname@users.noreply.github.com>
2024-04-27 21:42:11 +0500

    Update vars.zsh

--- .config/zsh/vars.zsh
+++ .config/zsh/vars.zsh
@@ -4,8 +4,9 @@ export PYTHONSTARTUP=~/.config/python/pythonrc
 export RCLONE_PROGRESS=true                                                      export RCLONE_HUMAN_READABLE=true
 export NNN_BMS='d:~/storage/downloads;g:~/storage/pictures/Gallery/owner;m:~/storage/music;E:/data/data/com.termux/files/usr/etc;e:~/.config;D:~/storage/dcim;'
 export NNN_PLUG='f:fzopen;u:getplugs;i:-!|mediainfo "$nnn";l:-!bat --paging=always "$nnn"*;s:-!dua "$nnn";a:autojump;'
-BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
-export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+BLK="03" CHR="03" DIR="04" EXE="02" REG="07" HARDLINK="05" SYMLINK="05" MISSING="08" ORPHAN="01" FIFO="06" SOCK="03" UNKNOWN="01"
+export NNN_COLORS="#04020301;4231"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$UNKNOWN"
 export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lz
 h|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)$"
 export NNN_FIFO='/data/data/com.termux/files/usr/tmp/nnn.fifo'
